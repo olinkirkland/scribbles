6b0d3007e8b56c2a3694d5b597e5b358139bc6be
Olin Kirkland <me@olinkirk.land>
2023-12-11 17:47:18 +0100

    Scribbles deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: { host: false }, // For external IP access
+  base: '/scribbles',
   resolve: {
     alias: {
       '@': '/src'
     }
-  },
-  base: 'http://olinkirkland.github.io/scribbles'
+  }
 });
