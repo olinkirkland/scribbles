a3a192244e5d3029bd2835e139db9f7f9eced647
Olin Kirkland <me@olinkirk.land>
2026-06-03 14:57:46 -0400

    Build out the worldbuilding qs for heist-for-hire deluxe

--- public/data/_work-in-progress/deluxe-scores/html/assets/insert-header-footer.js
+++ public/data/_work-in-progress/deluxe-scores/html/assets/insert-header-footer.js
@@ -1,6 +1,6 @@
 const title = document.title
 
-const HEADER = `<header> <strong>${title}</strong> <img src="assets/images/header-decoration.png" /> <em>by Olin Kirkland</em> </header>`
+const HEADER = `<header> <strong>${title}</strong> <img src="assets/images/header-decoration.png" /> <strong>by Olin Kirkland</strong> </header>`
 const FOOTER = `<footer> <span> This work is based on Blades in the Dark <sub>https://www.bladesinthedark.com</sub>, product of One Seven Design, developed and authored by John Harper, and licensed for our use under the Creative Commons Attribution 3.0 Unported license <sub>https://www.creativecommons.org/licenses/by/3.0</sub> </span> <span> Find more scores and scenarios by Olin Kirkland <sub>https://www.olinkirk.land/scribbles</sub> </span> </footer>`
 
 const pages = document.querySelectorAll('.print-page')
