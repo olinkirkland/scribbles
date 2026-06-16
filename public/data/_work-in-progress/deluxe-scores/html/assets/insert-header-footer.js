f12c9c63e0592c84a71e29d956afcc3b61d9ce28
Olin Kirkland <me@olinkirk.land>
2026-06-16 12:15:52 -0400

    Styling changes, icon changes, add counter

--- public/data/_work-in-progress/deluxe-scores/html/assets/insert-header-footer.js
+++ public/data/_work-in-progress/deluxe-scores/html/assets/insert-header-footer.js
@@ -1,9 +1,11 @@
 const title = document.title
 
-const HEADER = `<header> <strong>${title}</strong> <img src="assets/images/header-decoration.png" /> <strong>by Olin Kirkland</strong> </header>`
-const FOOTER = `<footer> <span> This work is based on Blades in the Dark <sub>https://www.bladesinthedark.com</sub>, product of One Seven Design, developed and authored by John Harper, and licensed for our use under the Creative Commons Attribution 3.0 Unported license <sub>https://www.creativecommons.org/licenses/by/3.0</sub> </span> <span> Find more scores and scenarios by Olin Kirkland <sub>https://www.olinkirk.land/scribbles</sub> </span> </footer>`
+const HEADER = `<header> <strong>${title}</strong> <img src="assets/images/decoration-2.svg" /> <strong>by Olin Kirkland</strong> </header>`
+const FOOTER =
+    `<footer> <span> This work is based on <a href="https://www.bladesinthedark.com">Blades in the Dark</a>, a product of One&nbsp;Seven&nbsp;Design, <br />developed and authored by John&nbsp;Harper, and licensed for our use under the <a href="https://www.creativecommons.org/licenses/by/3.0">Creative&nbsp;Commons&nbsp;Attribution&nbsp;3.0&nbsp;Unported&nbsp;License</a> </span>` +
+    `<span> Find more content by Kirkland&nbsp;Press at <a href="https://www.olinkirk.land/press">https://www.olinkirk.land/press</a> </span> </footer>`
 
-const pages = document.querySelectorAll('.print-page')
+const pages = document.querySelectorAll('.page')
 
 pages[0].insertAdjacentHTML('afterbegin', HEADER)
 pages.forEach((page) => {
